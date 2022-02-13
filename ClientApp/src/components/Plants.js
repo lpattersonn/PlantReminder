3ec6dbdb9bcc94afb806d29a910117d50c3deb55
Leshan <leshan0627@gmail.com>
2022-02-13 16:03:38 -0500

    feat: Implement wellWatered fuction for changing table row color based on water staus

--- ClientApp/src/components/Plants.js
+++ ClientApp/src/components/Plants.js
@@ -3,16 +3,33 @@ import React from "react";
 export default function Plants(props) {
   console.log(props);
 
+  function wellWatered(waterInterval) {
+    if(waterInterval > 300) {
+      return true
+    }
+  }
+  
+  console.log(wellWatered(100))
+
   const plantList = props.plants.map((plant) => {
-    return (<tr>
-      <td>
-        <img src={plant.img} alt={plant.name} />
-      </td>
-      <td>{plant.name}</td>
-      <td>{plant.lastWatered}</td>
-      <td className="waterplants-button"><button>Water Me</button></td>
-    </tr>
-    )
+    
+    const waterStatus = { 
+      border: "0.2em solid black",
+      borderColor: wellWatered(plant.waterInterval) ? "#74E64E" : "#E6534E",
+    };
+
+    return (
+      <tr style={waterStatus}>
+        <td>
+          <img src={plant.img} alt={plant.name} />
+        </td>
+        <td style={waterStatus}>{plant.name}</td>
+        <td style={waterStatus}>{plant.lastWatered}</td>
+        <td className="waterplants-button">
+          <button>Water Me</button>
+        </td>
+      </tr>
+    );
   });
 
   return (
@@ -22,7 +39,9 @@ export default function Plants(props) {
           <th>Plants</th>
           <th>Plant Name</th>
           <th>Last Watered</th>
-          <th className="waterplants-button"><button>Water All Plants</button></th>
+          <th className="waterplants-button">
+            <button>Water All Plants</button>
+          </th>
         </tr>
       </thead>
       <tbody>{plantList}</tbody>
